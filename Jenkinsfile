ff58d890c3878ee8042b55e3f0580665fe1e9d77
michagonzo77 <michagonzo77@gmail.com>
2023-01-09 12:41:54 -0500

    added Jenkinsfile and deployment.yaml

---
+++ Jenkinsfile
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+    tools {
+        git 'Default'
+        nodejs 'node'
+    }
+    environment {
+        PATH = "${tool 'node'}/bin:${env.PATH}"
+    }
+    stages {
+        stage('Build') {
+            steps {
+                bat """set JENKINS_NODE_COOKIE=dontKillMe && start /min npm install """
+            }
+        }
+        stage('Test') {
+            steps {
+                bat """set JENKINS_NODE_COOKIE=dontKillMe && start /min npm test """
+            }
+        }
+        stage('Build and push Docker image') {
+            steps {
+                withDockerRegistry([credentialsId: '858b2025-94a5-4bfb-82d9-d9c96be7ec4c', url: 'https://index.docker.io/v1/']) {
+                    withDockerServer([credentialsId: '858b2025-94a5-4bfb-82d9-d9c96be7ec4c', uri: 'https://index.docker.io/v1/']) {
+                        bat """set JENKINS_NODE_COOKIE=dontKillMe && start /min docker build -t index.docker.io/v1/hello-world:latest . """
+                        bat """set JENKINS_NODE_COOKIE=dontKillMe && start /min docker push index.docker.io/v1/hello-world:latest """
+                    }
+                }
+            }
+        }
+        stage('Deploy to Kubernetes') {
+            steps {
+                withKubeConfig([credentialsId: 'f97b324c-556d-4676-8adf-7a5b36ece251', serverUrl: 'https://kubernetes.docker.internal:6443']) {
+                    bat """set JENKINS_NODE_COOKIE=dontKillMe && start /min kubectl apply -f deployment.yaml """
+                }
+            }
+        }
+    }
+}
--- app.js
+++ app.js
@@ -1,12 +1,90 @@
 const express = require('express')
+const promClient = require('prom-client')
 
 const app = express()
 
+// Create a Gauge for CPU usage
+const cpuUsage = new promClient.Gauge({
+    name: 'cpu_usage',
+    help: 'Current CPU usage'
+})
+
+// Create a Gauge for memory usage
+const memoryUsage = new promClient.Gauge({
+    name: 'memory_usage',
+    help: 'Current memory usage'
+})
+
+// Update the Gauges every second
+setInterval(() => {
+    // Update the CPU usage Gauge
+    const cpuUsageValue = getCpuUsage()
+    if (!isNaN(cpuUsageValue)) {
+        cpuUsage.set(cpuUsageValue)
+    }
+
+    // Update the memory usage Gauge
+    const memoryUsageValue = getMemoryUsage()
+    if (!isNaN(memoryUsageValue)) {
+        memoryUsage.set(memoryUsageValue)
+    }
+}, 1000)
+
+const requestDuration = new promClient.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['route'],
+    buckets: [0.1, 0.5, 1, 1.5]
+})
+
+app.use((req, res, next) => {
+    const start = Date.now()
+
+    res.on('finish', () => {
+        const elapsed = Date.now() - start
+        requestDuration.labels(req.route.path).observe(elapsed / 1000)
+    })
+
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', promClient.register.contentType)
+
+    try {
+        const metrics = await promClient.register.metrics()
+        res.end(metrics)
+    } catch (error) {
+        console.error(error)
+        res.status(500).end('Internal Server Error')
+    }
+})
+
 app.listen(3001, () => {
     console.log('Application listening on port 3001')
 })
+
+function getCpuUsage() {
+    // Get the current system load and the number of available CPUs
+    const load = os.loadavg()[0]
+    const cpus = os.cpus().length
+
+    // Calculate the current CPU usage as a percentage
+    const usage = Math.round((load / cpus) * 100)
+
+    return usage
+}
+
+function getMemoryUsage() {
+    // Get the current process memory usage
+    const usage = process.memoryUsage().heapUsed
+
+    // Convert the memory usage to megabytes
+    const usageMB = Math.round(usage / 1024 / 1024)
+
+    return usageMB
+}
