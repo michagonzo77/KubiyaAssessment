9d375af196665b5046e54c70c2c8e4108c1c36e3
michagonzo77 <michagonzo77@gmail.com>
2023-01-09 21:24:23 -0500

    used ngrok to forward github webhook to my jenkins server

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! Hola Amigos! How are ya?')
+    res.send('Hello, World! Hola Amigos! How are ya? This is working now.')
 })
 
 app.get('/metrics', async (req, res) => {
